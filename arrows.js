96f4c01156ac2b95d315c5ceae30ee0911fd7741
David Torres Ayala <david.torresayala@hotmail.com>
2023-01-18 21:05:25 +0100

    Iteracion#2

--- arrows.js
+++ arrows.js
@@ -7,10 +7,10 @@ por consola la suma de los dos parametros.
 let a = 10;
 let b = 5;
 // 1.1 Ejecuta esta función sin pasar ningún parametro
-// const suma = () =>{}
+    // const suma = () =>{}
 
 // 1.2 Ejecuta esta función pasando un solo parametro
-// const suma = a => {}
+    // const suma = a => {}
 
 // 1.3 Ejecuta esta función pasando dos parametros
 
