5f68a6be69140968442757e601abc18a41111ffd
David Torres Ayala <david.torresayala@hotmail.com>
2023-01-22 22:23:29 +0100

    Iteracion#5

---
+++ filter.js
@@ -0,0 +1,112 @@
+
+// 5.1 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+// con los valores que sean mayor que 18
+const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+
+const mayores = ages.filter(numeros => numeros >18)
+
+// console.log(mayores);
+
+// 5.2 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+// con los valores que sean par.
+const ages2 = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+
+const pares = ages2.filter(numeros => numeros%2==0)
+
+// console.log(pares);
+
+// 5.3 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+// con los streamers que tengan el gameMorePlayed = 'League of Legends'.
+const streamers1 = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'}, 
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+]
+
+const leyendas = streamers1.filter( influencers => influencers.gameMorePlayed==="League of Legends")
+
+// console.log(leyendas);
+
+// 5.4 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+// con los streamers que incluyan el caracter 'u' en su propiedad .name. Recomendamos 
+// usar la funcion .includes() para la comprobación.
+const streamers2 = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+const youtubers = streamers2.filter ( personas => personas.name.includes("u"))
+
+// console.log(youtubers);
+
+
+// 5.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan 
+// el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la funcion 
+// .includes() para la comprobación.
+// Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando 
+// .age sea mayor que 35.
+
+const streamers3 = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+const instagramers = streamers3.filter( person => person.gameMorePlayed.includes("Legends"))
+    
+    for (const filtrado of instagramers) {
+        if ( filtrado.age >35){
+            filtrado.gameMorePlayed = filtrado.gameMorePlayed.toUpperCase();
+        }
+     console.log(filtrado);   
+    }
+
+// 5.6 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
+// los streamers que incluyan la palabra introducida en el input. De esta forma, si 
+// introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si
+// introduzco 'i', me deberia de mostrar el streamer 'Rubius' e 'Ibai'.
+const streamers = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+const toFilterStreamers = document.querySelector("input");
+toFilterStreamers.innerHTML = "";
+
+toFilterStreamers = streamers.filter(gente => gente.name.includes(value));
+
+console.log(toFilterStreamers);
+
+
+
+// 5.7 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
+// los streamers que incluyan la palabra introducida en el input. De esta forma, si 
+// introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si introduzco 'i', 
+// me deberia de mostrar el streamer 'Rubius' e 'Ibai'.
+// En este caso, muestra solo los streamers filtrados cuando hagamos click en el button.
+// const streamers = [
+// 	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+// 	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+// 	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+// 	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+// ];
+
+// <!doctype html>
+// <html lang="en">
+// <head>
+// <meta charset="UTF-8">
+//              <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+//              <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//              <title>Document</title>
+// </head>
+// <body>
+//   <input type="text" data-function="toFilterStreamers"/>
+//   <button data-function="toShowFilterStreamers">Filter</button>
+// </body>
+// </html>
